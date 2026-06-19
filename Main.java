5995ba989ae706a4e19b835bb09eff711e2ac86c
avelez61 <velezaj13@protonmail.com>
2026-06-19 11:01:35 -0400

    Adds checkWin which covers 5 in a row for all directions + if the board is full

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
 	// Constants
@@ -8,11 +9,29 @@ public class Main {
 	static final String[] PLAYER_PIECES = {PLAYER_1_PIECE, PLAYER_2_PIECE};
 	static final int PLAYER_1 = 0;
 	static final int PLAYER_2 = 1;
-	static final int BOARD_SIZE = 15;
-	
+	static final int BOARD_SIZE = 15;	
+	static final HashMap<String, HashMap<String, Integer>> DIRECTIONS = new HashMap<>();
+
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		
+		// Set Direction Values
+		DIRECTIONS.put("HORIZONTAL", new HashMap<>());
+		DIRECTIONS.get("HORIZONTAL").put("ROW_CHANGE", 0);
+		DIRECTIONS.get("HORIZONTAL").put("COL_CHANGE", 1);
+
+		DIRECTIONS.put("VERTICAL", new HashMap<>());
+		DIRECTIONS.get("VERTICAL").put("ROW_CHANGE", 1);
+		DIRECTIONS.get("VERTICAL").put("COL_CHANGE", 0);
+
+		DIRECTIONS.put("ANTI_DIAGONAL", new HashMap<>());
+		DIRECTIONS.get("ANTI_DIAGONAL").put("ROW_CHANGE", 1);
+		DIRECTIONS.get("ANTI_DIAGONAL").put("COL_CHANGE", -1);
+
+		DIRECTIONS.put("DIAGONAL", new HashMap<>());
+		DIRECTIONS.get("DIAGONAL").put("ROW_CHANGE", 1);
+		DIRECTIONS.get("DIAGONAL").put("COL_CHANGE", 1);
+
 		// Init Board
 		String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
 		for (int r = 0; r < BOARD_SIZE; r++) {
@@ -22,8 +41,8 @@ public class Main {
 		}
 		int turn = PLAYER_1;
 		
-		boolean gameOver = false;
-		while (!gameOver) {
+		int winState = 0;
+		while (winState == 0) {
 			// Print Board
 			for (int r = 0; r < BOARD_SIZE; r++) {
 				for (int c = 0; c < BOARD_SIZE; c++) {
@@ -42,12 +61,57 @@ public class Main {
 			// Validate and Execute Player Move
 			if (inBounds(row, col) && board[row][col] == EMPTY) {
 				board[row][col] = PLAYER_PIECES[turn];
+				winState = checkWin(board, turn);
 				turn = turn == PLAYER_1 ? PLAYER_2 : PLAYER_1;
 			}
+		}
+		
+		// Print Board
+		for (int r = 0; r < BOARD_SIZE; r++) {
+			for (int c = 0; c < BOARD_SIZE; c++) {
+				System.out.print(board[r][c] + " ");
+			}
+			System.out.println();
 		}
 	}
 	
 	static boolean inBounds(int row, int col) {
 		return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE; 
+	}
+	
+	static int checkWin(String[][] board, int turn) {
+		String playerPiece = PLAYER_PIECES[turn];
+		boolean isFull = true;
+
+		for (int r = 0; r < BOARD_SIZE; r++) {
+			for (int c = 0; c < BOARD_SIZE; c++) {
+				for (HashMap<String, Integer> direction : DIRECTIONS.values()) {
+					int targetRow = r;
+					int targetCol = c;
+					int inARow = 0;
+
+					while (inBounds(targetRow, targetCol) && board[targetRow][targetCol].equals(playerPiece)) {
+						inARow++;
+						targetRow += direction.get("ROW_CHANGE");
+						targetCol += direction.get("COL_CHANGE");
+
+						if (inARow == 5) {
+							return turn + 1;
+						}
+					}
+				}
+
+				if (board[r][c] == EMPTY) {
+					isFull = false;
+				}
+			}
+		}
+		
+		if (isFull) {
+			return 3;
+		}
+		else {
+			return 0;
+		}
 	}
 }
