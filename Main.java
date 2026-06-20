a11b1a61d1b459bbba3f20120670763bde973c60
avelez61 <velezaj13@protonmail.com>
2026-06-19 23:54:57 -0400

    Restructures game logic into BoardPanel's addMouseListener and paintComponent methods

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
 import java.util.HashMap;
-
 import javax.swing.*;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
@@ -9,8 +7,22 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Dimension;
 
 public class Main extends JPanel {
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame("Gomoku");
+			BoardPanel boardPanel = new BoardPanel();
+			frame.add(boardPanel);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setVisible(true);
+		});
+	}
+}
+
+class BoardPanel extends JPanel {
 	// Constants
 	static final String EMPTY = "*";
 	static final String PLAYER_1_PIECE = "@";
@@ -18,21 +30,16 @@ public class Main extends JPanel {
 	static final String[] PLAYER_PIECES = {PLAYER_1_PIECE, PLAYER_2_PIECE};
 	static final int PLAYER_1 = 0;
 	static final int PLAYER_2 = 1;
-	static final int BOARD_SIZE = 15;	
+	static final int BOARD_SIZE = 15;
+	static final int SQUARE_SIZE = 32;
 	static final HashMap<String, HashMap<String, Integer>> DIRECTIONS = new HashMap<>();
+	private String[][] board;
+	private int turn;
+	private int winState;
 
-	public static void main(String args[]) {
-		SwingUtilities.invokeLater(() -> {
-			JFrame frame = new JFrame("Gomoku");
-			BoardPanel boardPanel = new BoardPanel();
-			frame.add(boardPanel);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(BOARD_SIZE * 32, BOARD_SIZE * 32);
-			frame.setVisible(true);
-		});
+	public BoardPanel() {
+		setPreferredSize(new Dimension(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE));
 
-		Scanner scanner = new Scanner(System.in);
-		
 		// Set Direction Values
 		DIRECTIONS.put("HORIZONTAL", new HashMap<>());
 		DIRECTIONS.get("HORIZONTAL").put("ROW_CHANGE", 0);
@@ -50,53 +57,62 @@ public class Main extends JPanel {
 		DIRECTIONS.get("DIAGONAL").put("ROW_CHANGE", 1);
 		DIRECTIONS.get("DIAGONAL").put("COL_CHANGE", 1);
 
-		// Init Board
-		String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
+		board = new String[BOARD_SIZE][BOARD_SIZE];
 		for (int r = 0; r < BOARD_SIZE; r++) {
 			for (int c = 0; c < BOARD_SIZE; c++) {
 				board[r][c] = EMPTY;
 			}
 		}
-		int turn = PLAYER_1;
-		
-		int winState = 0;
-		while (winState == 0) {
-			// Print Board
-			for (int r = 0; r < BOARD_SIZE; r++) {
-				for (int c = 0; c < BOARD_SIZE; c++) {
-					System.out.print(board[r][c] + " ");
+		turn = PLAYER_1;		
+		winState = 0;
+
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (winState != 0) { return; }
+
+				int col = (int) Math.floor((e.getX() / SQUARE_SIZE));
+				int row = (int) Math.floor((e.getY() / SQUARE_SIZE));
+
+				if (board[row][col] == EMPTY) {
+					board[row][col] = PLAYER_PIECES[turn];
+					winState = checkWin(board, turn);
+					turn = turn == PLAYER_1 ? PLAYER_2 : PLAYER_1;
+
+					repaint();
+
+					if (winState != 0) {
+						System.out.println("Win State: " + winState);
+					}
 				}
-				System.out.println();
 			}
-			
-			// Get Player Move
-			System.out.print("Enter a row to place a piece: ");
-			int row = Integer.parseInt(scanner.nextLine());
-			
-			System.out.print("Enter a col to place a piece: ");
-			int col = Integer.parseInt(scanner.nextLine());
-			
-			// Validate and Execute Player Move
-			if (inBounds(row, col) && board[row][col] == EMPTY) {
-				board[row][col] = PLAYER_PIECES[turn];
-				winState = checkWin(board, turn);
-				turn = turn == PLAYER_1 ? PLAYER_2 : PLAYER_1;
-			}
-		}
-		
-		// Print Board
+		});
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D graphics = (Graphics2D) g;
+
 		for (int r = 0; r < BOARD_SIZE; r++) {
 			for (int c = 0; c < BOARD_SIZE; c++) {
-				System.out.print(board[r][c] + " ");
+				if (board[r][c].equals(PLAYER_1_PIECE)) {
+					graphics.setColor(Color.BLACK);
+					graphics.fillOval(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+				}
+				else if (board[r][c].equals(PLAYER_2_PIECE)) {
+					graphics.setColor(Color.WHITE);
+					graphics.fillOval(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+				}
+				
 			}
-			System.out.println();
 		}
 	}
 
 	static boolean inBounds(int row, int col) {
 		return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE; 
 	}
-	
+
 	static int checkWin(String[][] board, int turn) {
 		String playerPiece = PLAYER_PIECES[turn];
 		boolean isFull = true;
@@ -131,25 +147,5 @@ public class Main extends JPanel {
 		else {
 			return 0;
 		}
-	}
-}
-
-class BoardPanel extends JPanel {
-	public BoardPanel() {
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int col = Math.round((e.getX() / (float) 32));
-				int row = Math.round((e.getY() / (float) 32));
-
-				System.out.println("ROW: " + row);
-				System.out.println("COL: " + col);
-			}
-		});
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		Graphics2D graphics = (Graphics2D) g;
 	}
 }
